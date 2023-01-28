16f82d06ae802c3f7f694c168c34fff0e0cb8b5e
Possibletoast <123704580+Possibletoast@users.noreply.github.com>
2023-01-28 14:03:35 -0500

    Add files via upload

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,100 +4,53 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+ * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  private DifferentialDrive m_myRobot;
+  private Joystick m_leftStick;
+  private Joystick m_rightStick;
 
-  private RobotContainer m_robotContainer;
+  private final MotorController m_leftMotora = new CANSparkMax(0);
+  private final MotorController m_rightMotora = new CANSparkMax(1);
+  private final MotorController m_leftMotorb = new CANSparkMax(0);
+  private final MotorController m_rightMotorb = new CANSparkMax(1);
+  varname NewLeftMotor = (leftMotora,leftmotorb);
+  varname NewRightMotor = (rightMotora,rightMotorb);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  m_leftmotor = m_leftmotorb(m_leftMotora,leftmotorb);
+  private final SpeedControllerGroup RightSpeedGroup = new SpeedControllerGroup(NewRightMotor);
+  private final SpeedControllerGroup LeftSpeedGroup = new SpeedControllerGroup(NewLeftMotor);
+  //drivetrain
+  DifferentialDrive drivetrain = new DifferentialDrive(LeftSpeedGroup,RightSpeedGroup);
+  //joystick
+varname Joystick stick = new Joysticka(0);
+Joystick stick = new Joystickb(1);
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_rightMotora.setInverted(true);
+    m_leftMotora.setInverted(isInverted:true);
+    m_leftMotorb.setInverted(isInverted:false);
+    m_rightmotorb.setInverted(isInverted:false);
+
+    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    m_leftStick = new Joystick(0);
+    m_rightStick = new Joystick(1);
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+  public void teleopPeriodic() {
+    m_myRobot.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
   }
-
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
 }
